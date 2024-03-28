8eaea048c6d6b8449d5ad1cf03c95d7a5f7b367a
tham97 <pntham97@gmail.ccom>
2024-03-28 09:55:43 +0700

    create UI create post

--- apps/web/src/pages/(auth)/_layout.tsx
+++ apps/web/src/pages/(auth)/_layout.tsx
@@ -1,17 +1,21 @@
+import { useQuery } from '@tanstack/react-query'
 import { Navigate, Outlet } from 'react-router-dom'
 import Header from './_components/Header'
-
+import { getMe } from '@/apis/auth'
 import { getToken } from '@/utils/token'
 
 export default function Component() {
+  const { data: meQuery } = useQuery({
+    queryKey: ['me'],
+    queryFn: getMe
+  })
   const accessToken = getToken()
   if (!accessToken) return <Navigate to="/login" />
-
   return (
     <div className="w-full">
       <Header />
       <div className=" pt-16">
-        <Outlet />
+        <Outlet context={{ me: meQuery?.data }} />
       </div>
     </div>
   )
