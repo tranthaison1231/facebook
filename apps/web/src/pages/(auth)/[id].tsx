8f4b67152f5d0483f9e267362bc6c00f3ce7979b
tham97 <pntham97@gmail.ccom>
2024-04-16 18:26:23 +0700

    upload image UI

--- apps/web/src/pages/(auth)/[id].tsx
+++ apps/web/src/pages/(auth)/[id].tsx
@@ -2,10 +2,14 @@ import { getUser } from '@/apis/auth'
 import { fetchPostsByUserId } from '@/apis/posts'
 import { useParams } from '@/router'
 import { useQuery } from '@tanstack/react-query'
-import { Pencil, Plus, Camera, Settings, Settings2, Text, Grid2X2 } from 'lucide-react'
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog'
+import { Pencil, Plus, Camera, Settings, Settings2, Text, Grid2X2, Scissors, Clock } from 'lucide-react'
+import { useState } from 'react'
 
 export default function Component() {
   const { id } = useParams('/:id')
+  const [open, setOpen] = useState(false)
+  const [avatar, setAvatar] = useState<string | null>(null)
   const { data: userQuery } = useQuery({
     queryKey: ['user', id],
     queryFn: () => getUser(id)
@@ -15,12 +19,18 @@ export default function Component() {
     queryKey: ['users', id, 'posts'],
     queryFn: () => fetchPostsByUserId(id)
   })
-
+  const handleEditAvata = () => {
+    setAvatar(userQuery?.data?.avatar)
+  }
+  const handleUploadImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // setAvatar(e.target.files?.[0] || null)
+    console.log(e.target.files?.[0])
+  }
   console.log(postsQuery?.data)
   return (
     <div>
       <div className="px-52">
-        <div className="relative">
+        <div className="relative mt-2">
           <img className="h-full max-h-96 w-full rounded-b-md object-cover " src={userQuery?.data?.background} alt="" />
           <button className="absolute bottom-4 right-6 flex gap-2 rounded border-none bg-white px-3 py-2 font-medium hover:bg-gray-200">
             {' '}
@@ -36,9 +46,195 @@ export default function Component() {
                 src={userQuery?.data?.avatar}
                 alt=""
               />
-              <div className=" absolute bottom-0 right-5  w-10 rounded-full bg-gray-300 p-2">
-                <Camera />
-              </div>
+              <Dialog open={open} onOpenChange={setOpen}>
+                <DialogTrigger asChild>
+                  <div className=" absolute bottom-0 right-5  w-10 rounded-full bg-gray-300 p-2">
+                    <Camera />
+                  </div>
+                </DialogTrigger>
+                <DialogContent className="h-full  overflow-scroll sm:max-w-[768px]">
+                  <div className="flex w-full items-center justify-center border-b-2 border-gray-200 py-4">
+                    <h2 className="text-2xl font-bold">Chọn ảnh đại diện</h2>
+                  </div>
+                  {avatar !== null && (
+                    <div>
+                      <div className=" border-b-2 border-gray-200 p-4">
+                        <textarea className="w-full p-4" name="" id="" placeholder="Mô tả"></textarea>
+                        <img className="mt-2 w-full" src={avatar} alt="" />
+                        <div className="my-4 flex w-full items-center justify-center gap-2">
+                          <button className="flex items-center justify-center gap-2 rounded-sm bg-gray-200 p-2">
+                            <Scissors className="h-4 w-4" />
+                            Cắt ảnh
+                          </button>
+                          <button className="flex items-center justify-center gap-2 rounded-sm bg-gray-200 p-2">
+                            <Clock className="h-4 w-4" />
+                            Để tạm thời
+                          </button>
+                        </div>
+                        <p>
+                          Hệ thống cũng sẽ cập nhật ảnh đại diện của bạn cho <span>Phạm ngọc Thắm</span> trên Instagram.
+                          <a className="text-blue-500" href="">
+                            Quản lý cài đặt đồng bộ
+                          </a>
+                        </p>
+                      </div>
+                      <div className=" flex items-center justify-end gap-2 px-4">
+                        <button className="text-base font-medium text-blue-800">Hủy</button>
+                        <button className="rounded-sm bg-blue-800 px-12 py-2 text-white">Lưu</button>
+                      </div>
+                    </div>
+                  )}
+                  {avatar === null && (
+                    <div>
+                      <div className=" my-4 flex w-full gap-2 px-4 ">
+                        <div className="relative  w-full">
+                          <label
+                            htmlFor="file"
+                            className=" z-10 flex w-full items-center justify-center gap-2 rounded-sm bg-blue-200 py-2 text-base font-medium text-blue-500"
+                          >
+                            <Plus /> Tải ảnh lên
+                            <input
+                              onChange={handleUploadImage}
+                              id="file"
+                              className=" absolute -z-10 h-full w-full rounded-sm "
+                              type="file"
+                            />
+                          </label>
+                        </div>
+                        <button className="w-full rounded-sm bg-gray-200 py-2 text-base font-medium">
+                          Thêm Khung{' '}
+                        </button>
+                        <button onClick={handleEditAvata} className="rounded-sm bg-gray-200 px-4 py-2">
+                          <Pencil />
+                        </button>
+                      </div>
+                      <div className="h-96 w-full overflow-scroll">
+                        <div className="px-4">
+                          <h3 className="text-lg font-medium">Ảnh gợi ý</h3>
+                          <div className="my-4 grid grid-cols-6 gap-2">
+                            <img
+                              className="min-h-20 object-cover"
+                              src="https://i.pinimg.com/originals/ce/12/25/ce1225f92e766b3a87113dc69560e88f.jpg"
+                              alt=""
+                            />
+                            <img
+                              className="min-h-20 object-cover"
+                              src="https://i.pinimg.com/originals/ce/12/25/ce1225f92e766b3a87113dc69560e88f.jpg"
+                              alt=""
+                            />
+                            <img
+                              className="min-h-20 object-cover"
+                              src="https://i.pinimg.com/originals/ce/12/25/ce1225f92e766b3a87113dc69560e88f.jpg"
+                              alt=""
+                            />
+                            <img
+                              className="min-h-20 object-cover"
+                              src="https://i.pinimg.com/originals/ce/12/25/ce1225f92e766b3a87113dc69560e88f.jpg"
+                              alt=""
+                            />
+                            <img
+                              className="min-h-20 object-cover"
+                              src="https://i.pinimg.com/originals/ce/12/25/ce1225f92e766b3a87113dc69560e88f.jpg"
+                              alt=""
+                            />
+                            <img
+                              className="min-h-20 object-cover"
+                              src="https://i.pinimg.com/originals/ce/12/25/ce1225f92e766b3a87113dc69560e88f.jpg"
+                              alt=""
+                            />
+                          </div>
+                          <button className="my-2 flex w-full items-center justify-center bg-gray-200  py-2">
+                            Xem thêm
+                          </button>
+                        </div>
+                        <div className="px-4">
+                          <h3 className="text-lg font-medium">Ảnh có mặt bạn</h3>
+                          <div className="my-4 grid grid-cols-6 gap-2">
+                            <img
+                              className="min-h-20 object-cover"
+                              src="https://i.pinimg.com/originals/ce/12/25/ce1225f92e766b3a87113dc69560e88f.jpg"
+                              alt=""
+                            />
+                            <img
+                              className="min-h-20 object-cover"
+                              src="https://i.pinimg.com/originals/ce/12/25/ce1225f92e766b3a87113dc69560e88f.jpg"
+                              alt=""
+                            />
+                            <img
+                              className="min-h-20 object-cover"
+                              src="https://i.pinimg.com/originals/ce/12/25/ce1225f92e766b3a87113dc69560e88f.jpg"
+                              alt=""
+                            />
+                            <img
+                              className="min-h-20 object-cover"
+                              src="https://i.pinimg.com/originals/ce/12/25/ce1225f92e766b3a87113dc69560e88f.jpg"
+                              alt=""
+                            />
+                            <img
+                              className="min-h-20 object-cover"
+                              src="https://i.pinimg.com/originals/ce/12/25/ce1225f92e766b3a87113dc69560e88f.jpg"
+                              alt=""
+                            />
+                            <img
+                              className="min-h-20 object-cover"
+                              src="https://i.pinimg.com/originals/ce/12/25/ce1225f92e766b3a87113dc69560e88f.jpg"
+                              alt=""
+                            />
+                          </div>
+                          <button className="my-2 flex w-full items-center justify-center bg-gray-200  py-2">
+                            Xem thêm
+                          </button>
+                        </div>
+                        <div className="px-4">
+                          <h3 className="text-lg font-medium">Ảnh đại diện</h3>
+                          <div className="my-4 grid grid-cols-6 gap-2">
+                            <img
+                              className="min-h-20 object-cover"
+                              src="https://i.pinimg.com/originals/ce/12/25/ce1225f92e766b3a87113dc69560e88f.jpg"
+                              alt=""
+                            />
+                            <img
+                              className="min-h-20 object-cover"
+                              src="https://i.pinimg.com/originals/ce/12/25/ce1225f92e766b3a87113dc69560e88f.jpg"
+                              alt=""
+                            />
+                            <img
+                              className="min-h-20 object-cover"
+                              src="https://i.pinimg.com/originals/ce/12/25/ce1225f92e766b3a87113dc69560e88f.jpg"
+                              alt=""
+                            />
+                            <img
+                              className="min-h-20 object-cover"
+                              src="https://i.pinimg.com/originals/ce/12/25/ce1225f92e766b3a87113dc69560e88f.jpg"
+                              alt=""
+                            />
+                            <img
+                              className="min-h-20 object-cover"
+                              src="https://i.pinimg.com/originals/ce/12/25/ce1225f92e766b3a87113dc69560e88f.jpg"
+                              alt=""
+                            />
+                            <img
+                              className="min-h-20 object-cover"
+                              src="https://i.pinimg.com/originals/ce/12/25/ce1225f92e766b3a87113dc69560e88f.jpg"
+                              alt=""
+                            />
+                          </div>
+                        </div>
+                        <div className="px-4">
+                          <h3 className="text-lg font-medium">Ảnh của bạn và Hồ quốc phát</h3>
+                          <div className="my-4 grid grid-cols-6 gap-2">
+                            <img
+                              className="min-h-20 object-cover"
+                              src="https://i.pinimg.com/originals/ce/12/25/ce1225f92e766b3a87113dc69560e88f.jpg"
+                              alt=""
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                </DialogContent>
+              </Dialog>
             </div>
             <div className="flex w-full items-end justify-between gap-4 pb-4">
               <div className="flex flex-col gap-1">
