d7a3681cef76690b1af2ca8789068b435dd18014
unknown <huynhchitrung020503@gmail.com>
2024-06-23 14:25:41 +0700

    23/6 ui create product

--- apps/server/prisma/seed.ts
+++ apps/server/prisma/seed.ts
@@ -4,14 +4,36 @@ import { faker } from "@faker-js/faker";
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.user.createMany({
+  // await prisma.user.createMany({
+  //   data: {
+  //     email: faker.internet.email(),
+  //     firstName: faker.name.firstName(),
+  //     lastName: faker.name.lastName(),
+  //     password: faker.internet.password(),
+  //   },
+  // });
+
+  const vehicles = await prisma.category.create({
     data: {
-      email: faker.internet.email(),
-      firstName: faker.name.firstName(),
-      lastName: faker.name.lastName(),
-      password: faker.internet.password(),
+      id: 'propertyrentals',
+      name: 'Tài sản cho thuê',
+      icon:"https://suckhoedoisong.qltns.mediacdn.vn/324455921873985536/2024/3/22/chung-cu-1711099332080108224776.jpeg",
+      subCategories: {
+        create: [
+          {
+            id: 'apartments-for-rent',
+            name: 'Căn hộ cho thuê',
+          },
+          {
+            id: 'condos-for-rent',
+            name: 'Chung cư',
+          }
+        ],
+      },
     },
   });
+
+  console.log({ vehicles });
 }
 main()
   .then(async () => {
